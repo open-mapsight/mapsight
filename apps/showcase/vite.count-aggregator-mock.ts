50355548d297aa29772e9b301843c942e8706c16
Paul Golmann <mail@pje-web.de>
2026-06-13 00:03:41 +0200

    showcase: small lint fix
    Signed-off-by: Paul Golmann <mail@pje-web.de>

--- apps/showcase/vite.count-aggregator-mock.ts
+++ apps/showcase/vite.count-aggregator-mock.ts
@@ -1,19 +1,17 @@
-import type {Connect, PreviewServer, ViteDevServer} from "vite";
+import type {PreviewServer, ViteDevServer} from "vite";
 
 import {createCountAggregatorMockMiddleware} from "./count-aggregator-mock/mock-api.ts";
 
-function useCountAggregatorMock(middlewares: Connect.Server): void {
-	middlewares.use(createCountAggregatorMockMiddleware());
-}
-
 export function countAggregatorMockPlugin() {
+	const middleware = createCountAggregatorMockMiddleware();
+
 	return {
 		name: "count-aggregator-mock-api",
 		configureServer(server: ViteDevServer) {
-			useCountAggregatorMock(server.middlewares);
+			server.middlewares.use(middleware);
 		},
 		configurePreviewServer(server: PreviewServer) {
-			useCountAggregatorMock(server.middlewares);
+			server.middlewares.use(middleware);
 		},
 	};
 }
