67c7107b9b48b05b0780bca8ba50d07c7fbf89c9
Paul Golmann <mail@pje-web.de>
2026-09-09 20:55:12 +0200

    fix(ui): render default place-action icons without :has()

--- packages/ui/src/js/components/place-actions/place-actions.tsx
+++ packages/ui/src/js/components/place-actions/place-actions.tsx
@@ -56,6 +56,10 @@ function classNames(
 	return parts.filter(Boolean).join(" ");
 }
 
+function ActionIcon({icon}: {icon?: ReactNode}): ReactElement {
+	return <span className="ms3-place-actions__icon">{icon}</span>;
+}
+
 type ActionChrome = {
 	title: string;
 	ariaLabel?: string;
@@ -204,9 +208,7 @@ function Share({
 				aria-label={chrome.ariaLabel}
 				onClick={onShare}
 			>
-				{icon ? (
-					<span className="ms3-place-actions__icon">{icon}</span>
-				) : null}
+				<ActionIcon icon={icon} />
 				{chrome.visible}
 			</T>
 			<a
@@ -287,9 +289,7 @@ function ShowOnMapButton({
 			aria-label={chrome.ariaLabel}
 			onClick={onShowOnMap}
 		>
-			{icon ? (
-				<span className="ms3-place-actions__icon">{icon}</span>
-			) : null}
+			<ActionIcon icon={icon} />
 			{chrome.visible}
 		</T>
 	);
@@ -342,9 +342,7 @@ function Navigate({
 				aria-controls={open ? menuId : undefined}
 				onClick={() => setOpen((current) => !current)}
 			>
-				{icon ? (
-					<span className="ms3-place-actions__icon">{icon}</span>
-				) : null}
+				<ActionIcon icon={icon} />
 				{chrome.visible}
 			</T>
 			<PopoverDialog
@@ -405,9 +403,7 @@ function Website({
 			rel="external noreferrer noopener"
 			target="_blank"
 		>
-			{icon ? (
-				<span className="ms3-place-actions__icon">{icon}</span>
-			) : null}
+			<ActionIcon icon={icon} />
 			{chrome.visible}
 		</T>
 	);
@@ -444,9 +440,7 @@ function Call({
 			title={tooltip}
 			aria-label={chrome.ariaLabel}
 		>
-			{icon ? (
-				<span className="ms3-place-actions__icon">{icon}</span>
-			) : null}
+			<ActionIcon icon={icon} />
 			{chrome.visible}
 			{hideTelephone ? (
 				<span className="ms3-visuallyhidden">{`: ${call.telephone}`}</span>
